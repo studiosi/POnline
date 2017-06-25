30e0e55d335452e0ef28c6f8e25b1184f9e17adf
masuokas <mattisuokas@wippies.com>
2017-06-26 00:13:12 +0300

    Ransac corrections

--- app/utils/Ransac.php
+++ app/utils/Ransac.php
@@ -21,55 +21,69 @@ Return:
     class RANSAC {
 
     private $iter = 0;
-    private $besterr = INF;
+    //private $besterr = INF;
+    private $besterr = 0;
     private $threshold = 2.5;     // Used to determine if error is good enough 
     private $inliersRatio = 0.7;  // To accept a model, atl least 70% of points must fit 
     private $k = 500;
     private $testinliers = array();
     private $testmodel; //model parameters fitted to maybeinliers
-    private $alsoinliers = array();
-    private $bestfit = array();
     
-    public static function ransacAlg($data) {
+    private $bestfit = array();
+    private $t = 10; // How far the point can be from the ellipse border
+    private $d = 0;
+    
+    public function ransacAlg($data) {
+        $this->d = count($data) * $this->inliersratio;
+        //$bestfit = array();
+        $iter = 0;
+        while ($iter < $this->k) {
+            $alsoinliers = array();
+            ImageController::debug_to_console("Iter: " . $iter );    
+            // Adding 5 elements to testinliers array
+            for ($i = 0; $i < 5; $i++) {
+                $r = rand($min = 0 , $max = count($data)-$i );
 
-    while ($iter < $k) {
+                if (!in_array($data[$r], $this->testinliers))
+                    $this->testinliers[$i] = $data[$r]; 
 
-        // Adding 5 elements to testinliers array
-        for ($i = 0; $i < 5; $i++) {
-            $r = rand($min = 0 , $max = count($data)-$i );
+            }
+
+            // Fitting ellipse
+            Equation::setfrompoints($this->testinliers);
+            $center = Equation::getCenter();
+            $axis = Equation::getAxisLength();
+
+            //for every point in data not in testinliers {
+            for ($i = 0; $i < count($data); $i++) {
+                $p = $data[$i];
+                $p2 =  Equation::pointOnEllipse($axis[0], $axis[1], $p);
+                $v = array('dx' => $p['x'] - $p2['x'], 'dy' => $p['y'] - $p2['y']);
+                $distancefromborder = hypot($v['dx'], $v['dy']);
+                //if point fits maybemodel with an error smaller than t add point to alsoinliers
+                if ($distancefromborder < $this->t) {
+                    array_push($alsoinliers, $p);
+                }
+            }
             
-            if (!in_array($data[$r], $this->testinliers))
-                $this->testinliers[$i] = $data[$r]; 
-            
+            //if the number of elements in alsoinliers is > d {
+            if (count($alsoinliers) > $d) {
+                // this implies that we may have found a good model
+                // now test how good it is
+                //$bettermodel = Equation::setfrompoints($this->alsoinliers); // model parameters fitted to all points in maybeinliers and alsoinliers
+                $bettermodel = $alsoinliers;
+                $thiserr = count($alsoinliers); //a measure of how well model fits these points
+                //if ($thiserr < $this->besterr) {
+                if ($thiserr > $this->besterr) {
+                    $this->bestfit = $bettermodel;
+                    $this->besterr = $this->thiserr;
+                }
+            }
+            $iter++;
         }
-
-        // Fitting ellipse
-        Equation::setfrompoints($this->testinliers);
-
         
-
-        //for every point in data not in testinliers {
-        for ($i = 0; $i < $data; $i++) {
-
-            //if point fits maybemodel with an error smaller than t add point to alsoinliers
-            if () {
-                array_push($this->alsoinliers, $point);
-            }
-        }
-        //if the number of elements in alsoinliers is > d {
-        if (count($this->alsoinliers) > d) {
-            // this implies that we may have found a good model
-            // now test how good it is
-            $bettermodel; // model parameters fitted to all points in maybeinliers and alsoinliers
-            $thiserr; //a measure of how well model fits these points
-            if ($this->thiserr < $this->besterr) {
-                $this->bestfit = $bettermodel;
-                $this->besterr = $this->thiserr;
-            }
-        }
-        //increment iterations
-    }
-    return $this->bestfit;
+        var_dump($this->bestfit);
+        return $this->bestfit;
     }
 
 }
