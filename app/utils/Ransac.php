47e48d6af740b395ca44e31e96fb919e22bfaed8
masuokas <mattisuokas@wippies.com>
2017-08-03 22:29:53 +0300

    Modified Ransac parameters

--- app/utils/Ransac.php
+++ app/utils/Ransac.php
@@ -23,33 +23,35 @@ Return:
     private $iter = 0;
     private $besterr = INF;
     private $threshold = 2.5;     // Used to determine if error is good enough 
-    private $inliersRatio = 0.7;  // To accept a model, atl least 70% of points must fit 
+    private $inliersRatio = 0.9;  // To accept a model, atl least 70% of points must fit 
     private $k = 500;
     private $testinliers = array();
     private $testmodel; //model parameters fitted to maybeinliers
     
     private $bestfit = array();
-    private $t = 10; // How far the point can be from the ellipse border
+    private $t = 20; // How far the point can be from the ellipse border
     private $d = 0;
     
     public function ransacAlg($data) {
-        $this->d = count($data) * $this->inliersratio;
+        $this->d = 5;
         //$bestfit = array();
-        
+        $this->threshold = count($data) * $this->inliersRatio;
         $iter = 0;
         while ($iter < $this->k) {
+            $runthreshold = 0; // Max amount of runs if the fitness doesn't get better
             $thiserr = 0;
-            $alsoinliers = array();
-            ImageController::debug_to_console("Iter: " . $iter );    
+            $alsoinliers = array();   
             // Adding 5 elements to testinliers array
-            for ($i = 0; $i < 5; $i++) {
-                $r = rand($min = 0 , $max = count($data)-$i );
-
-                if (!in_array($data[$r], $this->testinliers))
-                    $this->testinliers[$i] = $data[$r]; 
-
+            $j = 0;
+            while ($j < 5) {
+                $r = rand(0 , count($data)-1);
+                if (!in_array($data[$r], $this->testinliers)) {
+                    $this->testinliers[$j] = $data[$r]; 
+                    $j = $j + 1;
+                }
             }
-
+            
+            
             // Fitting ellipse
             Equation::setfrompoints($this->testinliers);
             $center = Equation::getCenter();
@@ -58,33 +60,43 @@ Return:
             //for every point in data not in testinliers {
             for ($i = 0; $i < count($data); $i++) {
                 $p = $data[$i];
-                $p2 =  Equation::pointOnEllipse($axis[0], $axis[1], $p);
-                $v = array('dx' => $p['x'] - $p2['x'], 'dy' => $p['y'] - $p2['y']);
-                $distancefromborder = hypot($v['dx'], $v['dy']);
-                $thiserr += $distancefromborder;
-                //if point fits maybemodel with an error smaller than t add point to alsoinliers
-                if ($distancefromborder < $this->t) {
-                    array_push($alsoinliers, $p);
+                if (!in_array($p, $this->testinliers)) {
+                    $p2 =  Equation::pointOnEllipse($axis[0], $axis[1], $p);
+                    $v = array('dx' => $p['x'] - $p2['x'], 'dy' => $p['y'] - $p2['y']);
+                    $distancefromborder = hypot($v['dx'], $v['dy']);
+                    $thiserr += $distancefromborder;
+                    //if point fits maybemodel with an error smaller than t add point to alsoinliers
+                    if ($distancefromborder < $this->t) {
+                        array_push($alsoinliers, $p);
+                    }
                 }
             }
             
+
+            
+            //$this->threshold = 0;
             //if the number of elements in alsoinliers is > d {
-            if (count($alsoinliers) > $d) {
+            if (count($alsoinliers) > $this->d) {
                 // this implies that we may have found a good model
                 // now test how good it is
                 //$bettermodel = Equation::setfrompoints($this->alsoinliers); // model parameters fitted to all points in maybeinliers and alsoinliers
                 $bettermodel = $alsoinliers;
                 //$thiserr = count($alsoinliers); //a measure of how well model fits these points
                 if ($thiserr < $this->besterr) {
+                    $runtreshold = 0;
                 //if ($thiserr > $this->besterr) {
                     $this->bestfit = $bettermodel;
                     $this->besterr = $thiserr;
+                    
                 }
             }
             $iter++;
+            $runthreshold++;
+            if (($runthreshold > 50) || count($this->bestfit) > $this->threshold) {
+                return $this->bestfit;
+            }
         }
         
-        var_dump($this->bestfit);
         return $this->bestfit;
     }
 
