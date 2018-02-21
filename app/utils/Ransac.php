b6c77c518d97fa543ec7ec1d3b7473d8fd762e36
masuokas <mattisuokas@wippies.com>
2018-02-21 20:58:47 +0200

    Comments and code formatting

--- app/utils/Ransac.php
+++ app/utils/Ransac.php
@@ -6,32 +6,27 @@ namespace TU\Utils;
     use TU\controllers\ImageController;
     
     
-/*
-Given:
-    data – a set of observed data points
-    model – a model that can be fitted to data points
-    n – the minimum number of data values required to fit the model
-    k – the maximum number of iterations allowed in the algorithm
-    t – a threshold value for determining when a data point fits a model
-    d – the number of close data values required to assert that a model fits well to data
-
-Return:
-    bestfit – model parameters which best fit the data (or nul if no good model is found)
-*/
+/**
+ * The RANSAC algorithm for finding best fit model for the ellipse 
+ * @author Matti Suokas
+ */
     
     class RANSAC {
-
+    // Stores the model's error value, which is the cumulative error amount of pixel
+    // distances
     private $besterr = INF;
     /*
      threshold, an unspecified parameter in the formal statement of the RANSAC paradigm, 
      is used as the basis for determining that an n subset of P has been found that implies 
      a sufficiently large consensus set to permit the algorithm to terminate.
      */
-    private $threshold = 2.5;     // Used to determine if error is good enough 2.5
+    // Used to determine the amount of needed pts, calculated in the function
+    // from data point count and the set inliersRatio.
+    private $threshold = 2.5;     
     private $inliersRatio = 0.5;  // To accept a model, atl least x% of points must fit 
     private $k = 500; // Amount of iterations
-    private $testinliers = array();
-    private $bestfit = array();
+    private $testinliers = array(); // The testinliers the model fits the data with
+    private $bestfit = array(); // Best fit of points
     private $t = 16; // How far the point can be from the ellipse border (px)
     private $d = 0;
     
@@ -54,59 +49,38 @@ Return:
                 $this->testinliers[$j] = $data[$j];
             } 
             
-            /*
-            while ($j < 5) {
-                $r = rand(0 , count($data)-1);
-                if (!in_array($data[$r], $this->testinliers)) {
-                    $this->testinliers[$j] = $data[$r]; 
-                    $j = $j + 1;
-                }
-            } */
-            
             // Fitting ellipse
             Equation::setfrompoints($this->testinliers); 
             $finditer = 0;
+            // If the testinlier's give an array with inf param value in the
+            // equation. Fit until a proper array of five points is found.
             while (Equation::getErr() == true) {
                 shuffle($data);
                 for ($j = 0; $j < 5; $j++) {
                     $this->testinliers[$j] = $data[$j];
                 } 
-                
-                /*$j = 0;
-                $this->testinliers = array();
-                $maxiter = 0;
-                while ($j < 5) {
-                $r = rand(0 , count($data)-1);
-                    if (!in_array($data[$r], $this->testinliers)) {
-                        $this->testinliers[$j] = $data[$r]; 
-                        $j = $j + 1;
-                    }
-                    $maxiter = $maxiter + 1;
-                    if ($maxiter > 50) {
-                        for ($j = 0; $j < 5; $j++) {
-                            $this->testinliers[$j] = $data[$j];
-                        }
-                        $j = 5;
-                        $finditer = 50;
-                    }
-                } */
                 Equation::setfrompoints($this->testinliers);
                 $finditer = $finditer + 1;
+                // If a better model isn't found in 50 iters, just continue
                 if ($finditer > 50) {
                     Equation::setErr();
                 }
             } 
             
+            // Get ellipse's center point
             $center = Equation::getCenter();
+            // Get ellipse's axis length
             $axis = Equation::getAxisLength();
             
             //for every point in data not in testinliers {
             for ($i = 0; $i < count($data); $i++) {
                 $p = $data[$i];
                 if (!in_array($p, $this->testinliers)) {
+                    // See how far the point is from the border
                     $p2 =  Equation::pointOnEllipse($axis[0], $axis[1], $p);
                     $v = array('dx' => $p['x'] - $p2['x'], 'dy' => $p['y'] - $p2['y']);
                     $distancefromborder = hypot($v['dx'], $v['dy']);
+                    // Add to the error the distance from the border
                     $thiserr += $distancefromborder;
                     //if point fits model with distance smaller than t, add point to alsoinliers
                     if ($distancefromborder < $this->t) {
@@ -115,14 +89,12 @@ Return:
                 }
             }
             
-            //$this->threshold = 0;
             //if the number of elements in alsoinliers is > d {
             if (count($alsoinliers) > $this->d) {
                 // this implies that we may have found a good model
                 // now test how good it is
-                //$bettermodel = Equation::setfrompoints($this->alsoinliers); // model parameters fitted to all points in maybeinliers and alsoinliers
                 $bettermodel = $alsoinliers;
-                
+                // If we found a new best model, save it to bestfit and reset runthreshold
                 if ($thiserr < $this->besterr) {
                     $runthreshold = 0;
                     $this->bestfit = $bettermodel;
@@ -133,11 +105,14 @@ Return:
             $iter++;
             
             $runthreshold++;
+            // Return current bestfit, if over 100 runs without a new bestfit
             if (($runthreshold > 100) || (count($this->bestfit) > $this->threshold)) {
                 return $this->bestfit;
             }
         }
+        // If we haven't found a bestfit, set bestfit as the five testinlier points
         if (empty($this->bestfit)) $this->bestfit = $this->testinliers;
+        // Return model's points which best fit the data 
         return $this->bestfit;
     }
     
