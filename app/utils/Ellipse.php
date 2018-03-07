25b247a85eb3d007bd4be216a1b2d5e570058b0b
masuokas <mattisuokas@wippies.com>
2018-03-07 13:29:01 +0200

    Comments

--- app/utils/Ellipse.php
+++ app/utils/Ellipse.php
@@ -13,7 +13,7 @@ use TU\controllers\ImageController;
  * @author Matti Suokas
  */
     
-    class Ellipse{
+    class Ellipse {
         // Just to help with the equation.php (should be moved back to there)
         // Ellipse::setA($A); & Ellipse::getA(); are unecessary
         private static $arrayA=0;
@@ -26,7 +26,9 @@ use TU\controllers\ImageController;
             return self::$arrayA;
         }
             
-        // 3x3 matrix helpers
+        /* 3x3 matrix helpers */
+        
+        // Return $B matrix determinant
 	public static function determinant($B) {
             return $B[0][0] * $B[1][1] * $B[2][2]
                 + $B[0][1] * $B[1][2] * $B[2][0]
@@ -36,7 +38,9 @@ use TU\controllers\ImageController;
                 - $B[0][0] * $B[1][2] * $B[2][1];
 	}
         
+        // Return inverse matrix of $B
         public static function inverse($B) {
+            // Get determinant of matrix $B
             $d = Ellipse::determinant($B);
             return [
                 [($B[1][1] * $B[2][2] - $B[1][2] * $B[2][1]) / $d,
@@ -51,6 +55,7 @@ use TU\controllers\ImageController;
                 ];
 	}
         
+        // Return the result matrix from matrix multiply $A × $B 
         public static function multiply($A, $B) {
             return [[$A[0][0] * $B[0][0] + $A[0][1] * $B[1][0] + $A[0][2] * $B[2][0],
             $A[0][0] * $B[0][1] + $A[0][1] * $B[1][1] + $A[0][2] * $B[2][1],
@@ -62,27 +67,34 @@ use TU\controllers\ImageController;
             $A[2][0] * $B[0][1] + $A[2][1] * $B[1][1] + $A[2][2] * $B[2][1],
             $A[2][0] * $B[0][2] + $A[2][1] * $B[1][2] + $A[2][2] * $B[2][2]]];
 	}
-
+        
+        // Return transpose matrix of matrix $B
 	public static function transpose($B) {
             return [[$B[0][0], $B[1][0], $B[2][0]],
             [$B[0][1], $B[1][1], $B[2][1]],
             [$B[0][2], $B[1][2], $B[2][2]]];
 	}
         
+        // Return matrix result of matrix $A + matrix $B
         public static function add($A, $B) {
             return [[$A[0][0] + $B[0][0], $A[0][1] + $B[0][1], $A[0][2] + $B[0][2]],
             [$A[1][0] + $B[1][0], $A[1][1] + $B[1][1], $A[1][2] + $B[1][2]],
             [$A[2][0] + $B[2][0], $A[2][1] + $B[2][1], $A[2][2] + $B[2][2]]];
 	}
 
-	public static function trace($A) { return $A[0][0] + $A[1][1] + $A[2][2]; }  
+        // Return sum of the elements on matrix $A main diagonal
+	public static function trace($A) { 
+            return $A[0][0] + $A[1][1] + $A[2][2];             
+        }  
 
+        // Return scaled matrix $A by a multiplier of $k
 	public static function scale($A, $k) {
             return [[$k * $A[0][0], $k * $A[0][1], $k * $A[0][2]],
             [$k * $A[1][0], $k * $A[1][1], $k * $A[1][2]],
             [$k * $A[2][0], $k * $A[2][1], $k * $A[2][2]]];
 	}
         
+        // Return eigenvalues of matrix $A
         public static function eigenvalues($A) {
             $q = Ellipse::trace($A) / 3;                                 
             $K = Ellipse::add($A, [[-$q, 0, 0],[0, -$q, 0],[0, 0, -$q]]);
@@ -102,6 +114,7 @@ use TU\controllers\ImageController;
             $q + 2 * $p * cos($phi + (4 * M_PI / 3))];
 	}
         
+        // Return the found nullspace of the matrix $G
         public static function nullspace($G) {
             $k1 = -$G[2][0]/$G[2][2];
             $k2 = -$G[2][1]/$G[2][2];
